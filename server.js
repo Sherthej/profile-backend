74e9cef9e521eec2da37bd0da8bc80b06d0c7ef5
Sherthej <sherlinthejasavarachan@gmail.com>
2025-07-18 21:39:55 +0530

    update commit

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://profile-frontend-omega.vercel.app',
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
